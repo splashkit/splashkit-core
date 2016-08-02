4e4efa93f4a902fc69259416cbc8112cadc26842
Jake Renzella <j@kerenzella.com>
2016-08-02 18:30:02 +1000

    NEW: Add database backend files

---
+++ coresdk/src/backend/database_driver.cpp
@@ -0,0 +1,9 @@
+//
+//  database_driver.cpp
+//  splashkit
+//
+//  Created by Jake Renzella on 2/08/2016.
+//  Copyright © 2016 Andrew Cain. All rights reserved.
+//
+
+#include "database_driver.hpp"
---
+++ coresdk/src/backend/database_driver.h
@@ -0,0 +1,14 @@
+//
+//  database_driver.hpp
+//  splashkit
+//
+//  Created by Jake Renzella on 2/08/2016.
+//  Copyright © 2016 Andrew Cain. All rights reserved.
+//
+
+#ifndef database_driver_hpp
+#define database_driver_hpp
+
+#include <stdio.h>
+
+#endif /* database_driver_hpp */
