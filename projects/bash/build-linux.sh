399590873d4def289f6b152849bc57874f957a9a
James Armstrong <itsjarm@gmail.com>
2016-08-10 12:20:48 +1000

    FIX: Linux build script will not run the tests automatically now.

--- projects/bash/build-linux.sh
+++ projects/bash/build-linux.sh
@@ -26,7 +26,3 @@ g++ -std=c++1y ${CORE_SDK_PATH}/src/test/*.cpp -I${CORE_SDK_PATH}/src/coresdk \
 
 echo "Copying resources"
 cp -r ${CORE_SDK_PATH}/src/test/Resources ./out/linux
-
-echo "Running tests"
-export LD_LIBRARY_PATH="out/linux/"
-./out/linux/runtests
