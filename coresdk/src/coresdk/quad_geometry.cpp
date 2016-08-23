0380b89ef4140064cf8cff1794cdd3b57952407d
Andrew Cain <acain@swin.edu.au>
2016-08-23 12:48:32 +1000

    NEW: Add start of quad geometry

---
+++ coresdk/src/coresdk/quad_geometry.cpp
@@ -0,0 +1,9 @@
+//
+//  quad_geometry.cpp
+//  splashkit
+//
+//  Created by Andrew Cain on 23/08/2016.
+//  Copyright © 2016 Andrew Cain. All rights reserved.
+//
+
+#include "quad_geometry.h"
---
+++ coresdk/src/coresdk/quad_geometry.h
@@ -0,0 +1,13 @@
+//
+//  quad_geometry.hpp
+//  splashkit
+//
+//  Created by Andrew Cain on 23/08/2016.
+//  Copyright © 2016 Andrew Cain. All rights reserved.
+//
+
+#ifndef quad_geometry_h
+#define quad_geometry_h
+
+
+#endif /* quad_geometry_h */
