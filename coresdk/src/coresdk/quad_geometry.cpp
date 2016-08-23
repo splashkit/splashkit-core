4d05f3c82af6509d111262669b778a603374e62e
Clancy Light <clancylight@gmail.com>
2016-08-23 13:16:44 +1000

    NEW: Add quad geometry quad_from

--- coresdk/src/coresdk/quad_geometry.cpp
+++ coresdk/src/coresdk/quad_geometry.cpp
@@ -7,3 +7,20 @@
 //
 
 #include "quad_geometry.h"
+#include "types.h"
+
+
+
+quad quad_from(float x_top_left,float y_top_left,float x_top_right,float y_top_right, float x_bottom_left,float y_bottom_left,float x_bottom_right,float y_bottom_right)
+{
+    quad result;
+    result.points[0].x = x_top_left;
+    result.points[0].y = y_top_left;
+    result.points[1].x = x_top_right;
+    result.points[1].y = y_top_right;
+    result.points[2].x = x_bottom_left;
+    result.points[2].y = y_bottom_left;
+    result.points[3].x = x_bottom_right;
+    result.points[3].y = y_bottom_right;
+    return result;
+}
--- coresdk/src/coresdk/quad_geometry.h
+++ coresdk/src/coresdk/quad_geometry.h
@@ -9,5 +9,9 @@
 #ifndef quad_geometry_h
 #define quad_geometry_h
 
+#include "types.h"
+
+
+quad quad_from(float x_top_left, float y_top_left, float x_top_right, float y_top_right, float x_bottom_left, float y_bottom_left, float x_bottom_right, float y_bottom_right );
 
 #endif /* quad_geometry_h */
