e143a6c8d4b1240136663964c3ecaca209776505
Andrew Cain <acain@swin.edu.au>
2016-08-23 23:04:32 +1000

    NEW: Start of sprites.

---
+++ coresdk/src/coresdk/sprites.cpp
@@ -0,0 +1,9 @@
+//
+//  sprites.cpp
+//  splashkit
+//
+//  Created by Andrew Cain on 23/08/2016.
+//  Copyright © 2016 Andrew Cain. All rights reserved.
+//
+
+#include "sprites.h"
---
+++ coresdk/src/coresdk/sprites.h
@@ -0,0 +1,14 @@
+//
+//  sprites.hpp
+//  splashkit
+//
+//  Created by Andrew Cain on 23/08/2016.
+//  Copyright © 2016 Andrew Cain. All rights reserved.
+//
+
+#ifndef sprites_h
+#define sprites_h
+
+
+
+#endif /* sprites_h */
