b0826864d9cdebd8aee01e4c5973ef1f20b1eb2f
Andrew Cain <acain@swin.edu.au>
2016-09-11 08:37:12 +1000

    DOCS: Fix object param docs in son

--- coresdk/src/coresdk/json.h
+++ coresdk/src/coresdk/json.h
@@ -210,7 +210,7 @@ namespace splashkit_lib
      *
      * @param j The `json` object where data will be inserted for the given key.
      * @param key The `string` key where data will be stored in the `json` object.
-     * @param value The value to be inserted into the `json` object.
+     * @param object The value to be inserted into the `json` object.
      *
      * @attribute class json
      * @attribute method add_object
