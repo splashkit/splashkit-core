18bd5489f20fdbe150ad2f4431efb08cffcd7054
Andrew Cain <acain@swin.edu.au>
2016-09-10 16:33:44 +1000

    DOCS: Add timer docs

--- coresdk/src/coresdk/timers.h
+++ coresdk/src/coresdk/timers.h
@@ -14,6 +14,10 @@ using namespace std;
 namespace splashkit_lib
 {
     /**
+     * Timers in SplashKit can be used to track the passing of time. In general
+     * you will create a timer, start it, then use it to track time by asking
+     * for the timer's ticks (milliseconds).
+     *
      * @attribute class Timer
      */
     typedef struct _timer_data *timer;
@@ -24,6 +28,9 @@ namespace splashkit_lib
      *
      * @attribute class timer
      * @attribute constructor true
+     *
+     * @param  name The name of the timer for resource tracking
+     * @return      A new timer.
      */
     timer create_timer(string name);
 
@@ -32,6 +39,8 @@ namespace splashkit_lib
      *
      * @attribute class timer
      * @attribute destructor true
+     *
+     * @param to_free The time to be released.
      */
     void free_timer(timer to_free);
 
@@ -48,22 +57,20 @@ namespace splashkit_lib
      *
      * @attribute class timers
      * @attribute method get_timer_named
+     *
+     * @param  name The name of the timer to fetch
+     * @return      Returns the timer fetched from SplashKit
      */
     timer timer_named(string name);
-
-    /**
-     * Checks if SplashKit is aware of the given timer.
-     *
-     * @attribute class timers
-     * @attribute method has_timer
-     */
-    bool has_timer(timer t);
 
     /**
      * Checks if SplashKit has a timer with the indicated name.
      *
      * @attribute class timers
      * @attribute method has_timer
+     *
+     * @param  name The name of the timer
+     * @return      True if SplashKit has created a timer with that name.
      */
     bool has_timer(string name);
 
@@ -74,6 +81,8 @@ namespace splashkit_lib
      *
      * @attribute class timer
      * @attribute method start
+     *
+     * @param to_start The timer
      */
     void start_timer(timer to_start);
 
@@ -82,6 +91,7 @@ namespace splashkit_lib
      * You can check how long has past since the timer was started using the
      * `timer_ticks` function.
      *
+     * @param name The name of the timer
      */
     void start_timer(string name);
 
@@ -91,12 +101,16 @@ namespace splashkit_lib
      *
      * @attribute class timer
      * @attribute method stop
+     *
+     * @param to_stop The timer
      */
     void stop_timer(timer to_stop);
 
     /**
      * Stop the timer. The time is reset to 0 and you must
      * recall start to begin the timer ticking again.
+     *
+     * @param name The name of the timer
      */
     void stop_timer(string name);
 
@@ -106,12 +120,16 @@ namespace splashkit_lib
      *
      * @attribute class timer
      * @attribute method pause
+     *
+     * @param to_pause The timer
      */
     void pause_timer(timer to_pause);
 
     /**
      * Pause the timer, getting ticks from a paused timer
      * will continue to return the same time.
+     *
+     * @param name The name of the timer
      */
     void pause_timer(string name);
 
@@ -120,11 +138,15 @@ namespace splashkit_lib
      *
      * @attribute class timer
      * @attribute method resume
+     *
+     * @param to_resume The timer
      */
     void resume_timer(timer to_resume);
 
     /**
      * Resumes the named timer.
+     *
+     * @param name The name of the timer
      */
     void resume_timer(string name);
 
@@ -133,11 +155,15 @@ namespace splashkit_lib
      *
      * @attribute class timer
      * @attribute method reset
+     *
+     * @param tmr The timer
      */
     void reset_timer(timer tmr);
 
     /**
      * Resets the named timer
+     *
+     * @param name The name of the timer
      */
     void reset_timer(string name);
 
@@ -148,6 +174,11 @@ namespace splashkit_lib
      *
      * @attribute class timer
      * @attribute getter ticks
+     *
+     * @param  to_get The timer
+     * @return        The number of milliseconds that have passed since the
+     *                timer was created (excluding the time the timer was
+     *                paused)
      */
     unsigned int timer_ticks(timer to_get);
 
@@ -155,32 +186,49 @@ namespace splashkit_lib
      * Gets the number of ticks (milliseconds) that have passed since the timer
      * was started/reset. When paused the timer's ticks will not advance until
      * the timer is once again resumed.
+     *
+     * @param  name The name of the Timer
+     * @return      The number of milliseconds that have passed since the
+     *              timer was created (excluding the time the timer was
+     *              paused)
      */
     unsigned int timer_ticks(string name);
-    
+
     /**
      * Indicates if the timer is paused.
      *
      * @attribute class timer
      * @attribute getter is_paused
+     *
+     * @param  to_get The timer
+     * @return        True if the timer is paused
      */
     bool timer_paused(timer to_get);
-    
+
     /**
      * Indicates if the timer is paused.
+     *
+     * @param  name   The name of the timer
+     * @return        True if the timer is paused
      */
     bool timer_paused(string name);
-    
+
     /**
      * Indicates if the timer is started.
      *
      * @attribute class timer
      * @attribute getter is_started
+     *
+     * @param  to_get The timer
+     * @return        True if the timer has been started
      */
     bool timer_started(timer to_get);
-    
+
     /**
      * Indicates if the timer is started.
+     *
+     * @param  name   The name of the timer
+     * @return        True if the timer has been started
      */
     bool timer_started(string name);
 }
