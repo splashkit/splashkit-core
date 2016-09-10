332b06132058a75a168d4d1333271d5759969d3d
Andrew Cain <acain@swin.edu.au>
2016-09-10 20:38:50 +1000

    DOCS: Existing trangle geometry functions

--- coresdk/src/coresdk/triangle_geometry.h
+++ coresdk/src/coresdk/triangle_geometry.h
@@ -13,8 +13,29 @@
 
 namespace splashkit_lib
 {
+    /**
+     * Generate a triangle from a set of points.
+     *
+     * @param  x1 The x coordinate for the first point
+     * @param  y1 The y coordinate for the first point
+     * @param  x2 The x coordinate for the second point
+     * @param  y2 The y coordinate for the second point
+     * @param  x3 The x coordinate for the third point
+     * @param  y3 The y coordinate for the third point
+     * @return    A triangle at the indicated points
+     *
+     * @attribute suffix  _from_coordinates
+     */
     triangle triangle_from(float x1, float y1, float x2, float y2, float x3, float y3);
 
+    /**
+     * Generate a triangle from a set of points.
+     *
+     * @param  p1 The first point of the triangle
+     * @param  p2 The second point of the triangle
+     * @param  p3 The third point of the triangle
+     * @return    A triangle with the indicated points
+     */
     triangle triangle_from(point_2d p1, point_2d p2, point_2d p3);
 }
 
