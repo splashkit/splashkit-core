3d31e9d356591caf0374f88d8200633d5ad20cfe
Andrew Cain <acain@swin.edu.au>
2016-09-13 09:03:38 +1000

    DOCS: Update mouse input docs

--- coresdk/src/coresdk/mouse_input.h
+++ coresdk/src/coresdk/mouse_input.h
@@ -29,62 +29,91 @@ namespace splashkit_lib
 
     /**
      * Returns The current window position of the mouse as a `Vector`
+     *
+     * @returns The offset from the window origin to the mouse.
      */
     vector_2d mouse_position_vector();
 
     /**
      * Returns the current window position of the mouse as a `Point2D`
+     *
+     * @returns The position of the mouse in the current window.
      */
     point_2d mouse_position();
 
     /**
      * Returns the current x value of the mouse's position.
+     *
+     * @returns The distance of the mouse from the left edge of the current
+     *          window location
      */
     float mouse_x();
 
     /**
      * Returns the current y value of the mouse's position.
+     *
+     * @returns The distance of the mouse from the top edge of the current
+     *          window location
      */
     float mouse_y();
 
     /**
      * Returns the amount of accumulated mouse movement, since the last time
-     * `ProcessEvents` was called, as a `Vector`.
+     * `process_events` was called, as a `vector_2d`.
+     *
+     * @returns The movement of the mouse since the last process events
      */
     vector_2d mouse_movement();
 
     /**
      * Returns the amount the mouse wheel was scrolled since the last call
-     * to `ProcessEvents`. The result is a vector containing the x and y
+     * to `process_events`. The result is a vector containing the x and y
      * amounts scrolled. Scroll left generates a negative x, scroll right a
      * positive x. Scroll backward is negative y, scroll forward positive y.
      * Note that on MacOS the directions may be inverted by OS settings.
+     *
+     * @returns The distance and direction of the mouse scroll since the last
+     *          `process_events`
      */
     vector_2d mouse_wheel_scroll();
 
     /**
-     * Returns ``true`` if the specified button is currently pressed down.
+     * Returns `true` if the specified button is currently pressed down.
+     *
+     * @param button  The mouse button to check
+     * @returns       True if the mouse button is down
      */
     bool mouse_down(mouse_button button);
 
     /**
-     * Returns ``true`` if the specified button is currently up.
+     * Returns `true` if the specified button is currently up.
+     *
+     * @param button  The mouse button to check
+     * @returns       True if the mouse button is up (i.e. not down)
      */
     bool mouse_up(mouse_button button);
 
     /**
      * Returns true if the specified button was clicked since the last time
-     * `ProcessEvents` was called
+     * `process_events` was called.
+     *
+     * @param button  The mouse button to check
+     * @returns       True if the mouse button was clicked
      */
     bool mouse_clicked(mouse_button button);
 
     /**
      * Moves the mouse cursor to the specified screen location.
+     *
+     * @param x   The new x location of the mouse
+     * @param y   The new y location of the mouse
      */
     void move_mouse(float x, float y);
 
     /**
      * Moves the mouse cursor to the specified screen location.
+     *
+     * @param point   The new location of the mouse
      */
     void move_mouse(point_2d point);
 
@@ -97,17 +126,19 @@ namespace splashkit_lib
     /**
      * Used to explicitly set the mouse cursors visible state (if it is showing
      * in the window or not) based on the show parameter.
+     *
+     * @param show  When true the mouse is shown, when false it is hidden
      */
     void show_mouse(bool show);
-    
+
     /**
      * Tells the mouse cursor to hide (no longer visible) if it is currently
      * showing. Use `ShowMouse` to make the mouse cursor visible again.
      */
     void hide_mouse();
-    
+
     /**
-     * Returns ``true`` if the mouse is currently visible, ``false`` if not.
+     * Returns `true` if the mouse is currently visible, `false` if not.
      */
     bool mouse_shown();
 }
