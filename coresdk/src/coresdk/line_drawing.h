24c34db65336e43a004434e7cd9b2cec7be5da15
Andrew Cain <acain@swin.edu.au>
2016-09-15 09:31:57 +1000

    NEW: Add additional line drawing functions

--- coresdk/src/coresdk/line_drawing.h
+++ coresdk/src/coresdk/line_drawing.h
@@ -43,7 +43,26 @@ namespace splashkit_lib
      *            point
      * @param opts The drawing options
      */
-    void draw_line(color clr, float x1, float y1, float x2, float y2, drawing_options opts);
+    void draw_line(color clr, float x1, float y1, float x2, float y2, const drawing_options &opts);
+
+    /**
+     * Draw a line from one point to another on the current window.
+     *
+     * @param clr       The color of the line
+     * @param from_pt   The start of the line
+     * @param to_pt     The end of the line
+     */
+    void draw_line(color clr, const point_2d &from_pt, const point_2d &to_pt);
+
+    /**
+     * Draw a line from one point to another on the current window.
+     *
+     * @param clr       The color of the line
+     * @param from_pt   The start of the line
+     * @param to_pt     The end of the line
+     * @param opts The drawing options
+     */
+    void draw_line(color clr, const point_2d &from_pt, const point_2d &to_pt, const drawing_options &opts);
 
     /**
      * Draws a line onto the current window.
