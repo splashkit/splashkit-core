7f8f5273d6067f6a8fd64a72edd1a8cbacce0d95
Andrew Cain <acain@swin.edu.au>
2016-09-15 09:24:52 +1000

    DOCS: Line drawing docs

--- coresdk/src/coresdk/line_drawing.h
+++ coresdk/src/coresdk/line_drawing.h
@@ -15,12 +15,51 @@
 namespace splashkit_lib
 {
     /**
+     * Draw a line from one point to another on the current window.
      *
+     * @param clr The color of the line
+     * @param x1  The distance from the left of the window/bitmap to the first
+     *            point
+     * @param y1  The distance from the top of the window/bitmap to the first
+     *            point
+     * @param x2  The distance from the left of the window/bitmap to the second
+     *            point
+     * @param y2  The distance from the top of the window/bitmap to the second
+     *            point
      */
     void draw_line(color clr, float x1, float y1, float x2, float y2);
+
+    /**
+     * Draw a line from one point to another
+     *
+     * @param clr The color of the line
+     * @param x1  The distance from the left of the window/bitmap to the first
+     *            point
+     * @param y1  The distance from the top of the window/bitmap to the first
+     *            point
+     * @param x2  The distance from the left of the window/bitmap to the second
+     *            point
+     * @param y2  The distance from the top of the window/bitmap to the second
+     *            point
+     * @param opts The drawing options
+     */
     void draw_line(color clr, float x1, float y1, float x2, float y2, drawing_options opts);
 
+    /**
+     * Draws a line onto the current window.
+     *
+     * @param clr The color of the line
+     * @param l   The line's details
+     */
     void draw_line(color clr, const line &l);
+
+    /**
+     * Draws a line.
+     *
+     * @param clr The color of the line
+     * @param l   The line's details
+     * @param opts The drawing options
+     */
     void draw_line(color clr, const line &l, drawing_options opts);
 }
 #endif /* line_drawing_h */
