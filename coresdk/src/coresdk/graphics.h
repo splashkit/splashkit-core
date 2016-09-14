0a3a1967cf089b3024fd8be23bbacdfc5ec9943b
Andrew Cain <acain@swin.edu.au>
2016-09-15 08:59:46 +1000

    DOCS: Add docs for graphics

--- coresdk/src/coresdk/graphics.h
+++ coresdk/src/coresdk/graphics.h
@@ -18,6 +18,7 @@
 #include "ellipse_drawing.h"
 #include "line_drawing.h"
 #include "clipping.h"
+
 namespace splashkit_lib
 {
     //---------------------------------------------------------------------------------------------------------
@@ -70,38 +71,93 @@ namespace splashkit_lib
      */
     int screen_height();
 
-    /// Saves the current screen a bitmap file. The file will be saved into the
-    /// current directory.
-    ///
-    /// @param basename   The base name for the screen shot. e.g. "GemCollector"
-    ///
-    /// Side Effects:
-    /// - Saves the current screen image to a bitmap file.
-    ///
-    /// @lib TakeScreenshot
+    /**
+     *  Saves a screenshot of the current window to a bitmap file. The file will
+     *  be saved onto the user's desktop.
+     *
+     * @param basename The base of the filename. If there is a file of this name
+     *                 already, then the name will be changed to generate a
+     *                 unique filename.
+     */
     void take_screenshot(const string &basename);
 
     /**
+     *  Saves a screenshot of the current window to a bitmap file. The file will
+     *  be saved onto the user's desktop.
      *
+     * @param wind     The window to capture in the screenshot
+     * @param basename The base of the filename. If there is a file of this name
+     *                 already, then the name will be changed to generate a
+     *                 unique filename.
      */
     void take_screenshot(window wind, const string &basename);
 
+    /**
+     * Save the bitmap to the user's desktop.
+     *
+     * @param bmp      The bitmap to save
+     * @param basename The base of the filename. If there is a file of this name
+     *                 already, then the name will be changed to generate a
+     *                 unique filename.
+     */
     void save_bitmap(bitmap bmp, const string &basename);
 
+    /**
+     * Returns the number of physical displays attached to the computer.
+     *
+     * @return The number of displays attached to the computer
+     */
     int number_of_displays();
-    
+
+    /**
+     * Returns the details about one of the displays attached to the computer.
+     *
+     * @param  index The display number (from 0 to `number_of_displays` - 1)
+     * @return       The details of this display
+     */
     display display_details(unsigned int index);
-    
+
+    /**
+     * Return the name of the display, read from the system details.
+     *
+     * @param  disp The display details
+     * @return      The name of the display
+     */
     string display_name(display disp);
-    
+
+    /**
+     * Return the width of the display in pixels, read from the system details.
+     *
+     * @param  disp The display details
+     * @return      The width of the display
+     */
     int display_width(display disp);
-    
+
+    /**
+     * Return the height of the display in pixels, read from the system details.
+     *
+     * @param  disp The display details
+     * @return      The height of the display
+     */
     int display_height(display disp);
-    
+
+    /**
+     * Return a relative x location for the display in pixels, read from the
+     * system details. This can be used to work out the arrangement of displays.
+     *
+     * @param  disp The display details
+     * @return      The x location of the display
+     */
     int display_x(display disp);
-    
+
+    /**
+     * Return a relative y location for the display in pixels, read from the
+     * system details. This can be used to work out the arrangement of displays.
+     *
+     * @param  disp The display details
+     * @return      The y location of the display
+     */
     int display_y(display disp);
-    
-    
+
 }
 #endif /* graphics_hpp */
