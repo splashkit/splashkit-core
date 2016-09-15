dc1f6be193ef2efaae4b3977c2a1566223ca9d64
Andrew Cain <acain@swin.edu.au>
2016-09-15 18:39:24 +1000

    DOCS: Add docs to util functions

--- coresdk/src/coresdk/utils.h
+++ coresdk/src/coresdk/utils.h
@@ -17,16 +17,22 @@
 
 namespace splashkit_lib
 {
-    /// Puts the program to sleep for a specified number of
-    /// milliseconds.
-    ///
     /**
+     * Puts the program to sleep for a specified number of milliseconds. If this
+     * is larger than 1 second, SplashKit will check to see if the user tries to
+     * quit during the delay. If the user does quit, the delay function returns
+     * without waiting.
      *
+     * @param milliseconds  The number of milliseconds to wait
      */
     void delay(unsigned int milliseconds);
 
-    /// Gets the number of milliseconds that have passed since the progrma was started.
-    ///
+    /**
+     * Gets the number of milliseconds that have passed since the program was
+     * started.
+     *
+     * @return The number of milliseconds passed
+     */
     unsigned int current_ticks();
 
     /**
