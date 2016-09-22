818b5794258a996e9fcafb89ffac8ab543872c37
Jake Renzella <j@kerenzella.com>
2016-09-22 19:34:17 +1000

    QUALITY: Move to map style passing

--- coresdk/src/test/test_twitter.cpp
+++ coresdk/src/test/test_twitter.cpp
@@ -17,14 +17,14 @@ using namespace splashkit_lib;
 void run_twitter_test()
 {
     json twitter_post = create_json();
-    json_add_string(twitter_post, "oauth_consumer_key", "VlU5u3eZD8O9FLLjCJ7X7SZCm");
-    json_add_string(twitter_post, "oauth_consumer_secret", "JdmKx1uYTFB5RL3Whbg6pyppQXGKt2NrHtmWQgWID37fWDaadb");
     
-    json_add_string(twitter_post, "oauth_token", "777822290318757888-ZTefOHbqKd1huzc7cf5BLDZwfT36rgG");
-    json_add_string(twitter_post, "oauth_token_secret", "gHT5YN7biHTVhOpK4AT4tsxZRVODDNL92iiAT8igE2FUf");
+    // Keep these secret
+    json_set_string(twitter_post, "oauth_consumer_key", "VlU5u3eZD8O9FLLjCJ7X7SZCm");
+    json_set_string(twitter_post, "oauth_consumer_secret", "JdmKx1uYTFB5RL3Whbg6pyppQXGKt2NrHtmWQgWID37fWDaadb");
+    json_set_string(twitter_post, "oauth_token", "777822290318757888-ZTefOHbqKd1huzc7cf5BLDZwfT36rgG");
+    json_set_string(twitter_post, "oauth_token_secret", "gHT5YN7biHTVhOpK4AT4tsxZRVODDNL92iiAT8igE2FUf");
     
+    json_set_string(twitter_post, "status", "#omg I'm so full! #killerkebabs #abrakebabra");
     
-    json_add_string(twitter_post, "status", "Test_POST plz IgnO,r34324fewioqk0e-w :) #SplashKit @wow");
-    
-    http_response response = new_tweet(twitter_post);
+    new_tweet(twitter_post);
 }
