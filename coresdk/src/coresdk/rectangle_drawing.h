0139cba5147b272190323832de98d6ccd8b593e6
Jake Renzella <j@kerenzella.com>
2017-01-16 10:22:01 +1100

    NEW: Add bitmap drawing options for rectangle

--- coresdk/src/coresdk/rectangle_drawing.h
+++ coresdk/src/coresdk/rectangle_drawing.h
@@ -53,7 +53,7 @@ namespace splashkit_lib
      */
     void draw_rectangle(color clr, const rectangle &rect, const drawing_options &opts);
 
-    /**
+    /**git stat
      * Draw a rectangle onto the current window.
      *
      * @param clr     The color of the rectangle
@@ -247,8 +247,6 @@ namespace splashkit_lib
      */
     void fill_quad_on_window(window destination, color clr, const quad &q, const drawing_options &opts);
     
-    
-    
     /**
      *  Draw a rectangle to the window using the supplied drawing options. The rectangle is centred on its x, y
      *  coordinates, and has the provided width and height.
@@ -315,5 +313,169 @@ namespace splashkit_lib
      * @attribute method    draw_rectangle
      */
     void draw_rectangle_on_window(window destination, color clr, const rectangle &rect);
+    
+    /**
+     * Fills a rectangle on the supplied bitmap, using the supplied drawing options.
+     *
+     * @param destination   The destination bitmap
+     * @param clr     The color of the rectangle
+     * @param x       The distance from the left of the bitmap/bitmap to the
+     *                rectangle
+     * @param y       The distance from the top of the bitmap/bitmap to the
+     *                rectangle
+     * @param width   The width of the rectangle
+     * @param height  The height of the rectangle
+     * @param opts    The drawing options
+     *
+     * @attribute suffix  with_options
+     */
+    void fill_rectangle_on_bitmap(bitmap destination, color clr, float x, float y, float width, float height, const drawing_options &opts);
+    
+    /**
+     * Fills a rectangle on the supplied bitmap to the current bitmap.
+     *
+     * @param destination   The destination bitmap
+     * @param clr     The color of the rectangle
+     * @param x       The distance from the left of the bitmap/bitmap to the
+     *                rectangle
+     * @param y       The distance from the top of the bitmap/bitmap to the
+     *                rectangle
+     * @param width   The width of the rectangle
+     * @param height  The height of the rectangle
+     */
+    void fill_rectangle_on_bitmap(bitmap destination, color clr, float x, float y, float width, float height);
+    
+    /**
+     * Fills a rectangle on the supplied bitmap using the supplied drawing options.
+     *
+     * @param destination   The destination bitmap
+     * @param clr     The color of the rectangle
+     * @param rect    The rectangle to draw
+     * @param opts    The drawing options
+     *
+     * @attribute suffix  record_with_options
+     */
+    void fill_rectangle_on_bitmap(bitmap destination, color clr, const rectangle &rect, const drawing_options &opts);
+    
+    /**
+     * Fill a rectangle on the supplied bitmap onto the current bitmap.
+     *
+     * @param destination   The destination bitmap
+     * @param clr     The color of the rectangle
+     * @param rect    The rectangle to draw
+     *
+     * @attribute suffix  record
+     */
+    void fill_rectangle_on_bitmap(bitmap destination, color clr, const rectangle &rect);
+    
+    /**
+     * Draw a quad on the supplied bitmap to the current bitmap.
+     *
+     * @param destination   The destination bitmap
+     * @param clr   The color for the quad
+     * @param q     The details of the quad
+     */
+    void draw_quad_on_bitmap(bitmap destination, color clr, const quad &q);
+    
+    /**
+     * Draw a quad on the supplied bitmap using the supplied drawing options.
+     *
+     * @param destination   The destination bitmap
+     * @param clr   The color for the quad
+     * @param q     The details of the quad
+     * @param opts  The drawing options
+     *
+     * @attribute suffix  with_options
+     */
+    void draw_quad_on_bitmap(bitmap destination, color clr, const quad &q, const drawing_options &opts);
+    
+    /**
+     * Fill a quad on the supplied bitmap on the current bitmap.
+     *
+     * @param destination   The destination bitmap
+     * @param clr   The color for the quad
+     * @param q     The details of the quad
+     */
+    void fill_quad_on_bitmap(bitmap destination, color clr, const quad &q);
+    
+    /**
+     * Fill a quad on the supplied bitmap using the supplied drawing options.
+     *
+     * @param destination   The destination bitmap
+     * @param clr The color for the quad
+     * @param q   The details of the quad
+     * @param opts  The drawing options
+     *
+     * @attribute suffix  with_options
+     */
+    void fill_quad_on_bitmap(bitmap destination, color clr, const quad &q, const drawing_options &opts);
+    
+    /**
+     *  Draw a rectangle to the bitmap using the supplied drawing options. The rectangle is centred on its x, y
+     *  coordinates, and has the provided width and height.
+     *
+     * @param destination   The destination bitmap
+     * @param clr    The color of the rectangle
+     * @param x      The x location of the rectangle
+     * @param y      The y location of the rectangle
+     * @param width  The width of the rectangle
+     * @param height The height of the rectangle
+     * @param opts   Drawing options to configure the drawing operation
+     *
+     * @attribute suffix    with_options
+     * @attribute class     bitmap
+     * @attribute method    draw_rectangle
+     */
+    void draw_rectangle_on_bitmap(bitmap destination, color clr, float x, float y, float width, float height, drawing_options opts);
+    
+    /**
+     *  Draw a rectangle to the bitmap using. The rectangle is centred on its x, y
+     *  coordinates, and has the provided width and height.
+     *
+     * @param destination   The destination bitmap
+     * @param clr    The color of the rectangle
+     * @param x      The x location of the rectangle
+     * @param y      The y location of the rectangle
+     * @param width  The width of the rectangle
+     * @param height The height of the rectangle
+     *
+     * @attribute class     bitmap
+     * @attribute method    draw_rectangle
+     */
+    void draw_rectangle_on_bitmap(bitmap destination, color clr, float x, float y, float width, float height);
+    
+    /**
+     *  Draw a rectangle to the bitmap using the supplied rect and drawing options. The rectangle is centred on its x, y
+     *  coordinates, and has the provided width and height.
+     *
+     * @param destination   The destination bitmap
+     * @param clr    The color of the rectangle
+     * @param x      The x location of the rectangle
+     * @param y      The y location of the rectangle
+     * @param rect   the rectangle to be drawn to the bitmap
+     * @param opts   Drawing options to configure the drawing operation
+     *
+     * @attribute suffix    record_with_options
+     * @attribute class     bitmap
+     * @attribute method    draw_rectangle
+     */
+    void draw_rectangle_on_bitmap(bitmap destination, color clr, const rectangle &rect, const drawing_options &opts);
+    
+    /**
+     *  Draw a rectangle to the bitmap using the supplied rect. The rectangle is centred on its x, y
+     *  coordinates, and has the provided width and height.
+     *
+     * @param destination   The destination bitmap
+     * @param clr    The color of the rectangle
+     * @param x      The x location of the rectangle
+     * @param y      The y location of the rectangle
+     * @param rect   the rectangle to be drawn to the bitmap
+     *
+     * @attribute suffix    record_with_options
+     * @attribute class     bitmap
+     * @attribute method    draw_rectangle
+     */
+    void draw_rectangle_on_bitmap(bitmap destination, color clr, const rectangle &rect);
+    
 }
 #endif /* rectangle_drawing_hpp */
