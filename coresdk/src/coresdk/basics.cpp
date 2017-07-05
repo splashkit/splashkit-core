083bfd16f54ded9c0ca04d820582c8dc29174874
Andrew Cain <macite@gmail.com>
2017-07-05 20:50:49 +1000

    FIX: Move basics into splashkit lib namespace

--- coresdk/src/coresdk/basics.cpp
+++ coresdk/src/coresdk/basics.cpp
@@ -15,75 +15,78 @@
 #include <cctype>
 #include <locale>
 
-
-// trim see: https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
-
-// trim from start
-string ltrim(const string &text)
+namespace splashkit_lib
 {
-    string s = text;
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-    return s;
-}
 
-// trim from end
-string rtrim(const string &text)
-{
-    string s = text;
-    s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-    return s;
-}
+    // trim see: https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
 
-// trim from both ends
-string trim(const string &text)
-{
-    return ltrim(rtrim(text));
-}
+    // trim from start
+    string ltrim(const string &text)
+    {
+        string s = text;
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+        return s;
+    }
 
-string to_lowercase(const string &text)
-{
-    string data = text;
-    std::transform(data.begin(), data.end(), data.begin(), ::tolower);
-    return data;
-}
+    // trim from end
+    string rtrim(const string &text)
+    {
+        string s = text;
+        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+        return s;
+    }
 
-string to_uppercase(const string &text)
-{
-    string data = text;
-    std::transform(data.begin(), data.end(), data.begin(), ::toupper);
-    return data;
-}
+    // trim from both ends
+    string trim(const string &text)
+    {
+        return ltrim(rtrim(text));
+    }
 
-// integer check see: https://stackoverflow.com/questions/2844817/how-do-i-check-if-a-c-string-is-an-int#2845275
+    string to_lowercase(const string &text)
+    {
+        string data = text;
+        std::transform(data.begin(), data.end(), data.begin(), ::tolower);
+        return data;
+    }
 
-bool is_integer(const string &text)
-{
-    string s = trim(text);
-    if(s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false;
-    
-    char * p;
-    strtol(s.c_str(), &p, 10);
-    
-    return (*p == 0);
-}
+    string to_uppercase(const string &text)
+    {
+        string data = text;
+        std::transform(data.begin(), data.end(), data.begin(), ::toupper);
+        return data;
+    }
 
-bool is_number(const string &text)
-{
-    string s = trim(text);
-    if(s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false;
-    
-    char * p;
-    strtod(s.c_str(), &p);
-    
-    return (*p == 0);
-}
+    // integer check see: https://stackoverflow.com/questions/2844817/how-do-i-check-if-a-c-string-is-an-int#2845275
 
-int convert_to_integer(const string &text)
-{
-    return std::stoi( text );
-}
+    bool is_integer(const string &text)
+    {
+        string s = trim(text);
+        if(s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false;
+        
+        char * p;
+        strtol(s.c_str(), &p, 10);
+        
+        return (*p == 0);
+    }
 
-double convert_to_double(const string &text)
-{
-    return std::stod( text );
+    bool is_number(const string &text)
+    {
+        string s = trim(text);
+        if(s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false;
+        
+        char * p;
+        strtod(s.c_str(), &p);
+        
+        return (*p == 0);
+    }
+
+    int convert_to_integer(const string &text)
+    {
+        return std::stoi( text );
+    }
+
+    double convert_to_double(const string &text)
+    {
+        return std::stod( text );
+    }
 }
--- coresdk/src/coresdk/basics.h
+++ coresdk/src/coresdk/basics.h
@@ -15,67 +15,71 @@
 
 using std::string;
 
-/**
- * Return a new string that removes the spaces from the start and end of
- * the input string.
- *
- * @param  text The string to trim.
- * @return      A new string with the contents of `text`, after removing spaces
- *                from the start and end.
- */
-string trim(const string &text);
+namespace splashkit_lib
+{
+    
+    /**
+     * Return a new string that removes the spaces from the start and end of
+     * the input string.
+     *
+     * @param  text The string to trim.
+     * @return      A new string with the contents of `text`, after removing spaces
+     *                from the start and end.
+     */
+    string trim(const string &text);
 
-/**
- * Return a lowercase version of the passed in string.
- *
- * @param  text The text to convert.
- * @return      A lower case version of the passed in text.
- */
-string to_lowercase(const string &text);
+    /**
+     * Return a lowercase version of the passed in string.
+     *
+     * @param  text The text to convert.
+     * @return      A lower case version of the passed in text.
+     */
+    string to_lowercase(const string &text);
 
-/**
- * Return a UPPERCASE version of the passed in string.
- *
- * @param  text The text to convert.
- * @return      An upper case version of the passed in text.
- */
-string to_uppercase(const string &text);
+    /**
+     * Return a UPPERCASE version of the passed in string.
+     *
+     * @param  text The text to convert.
+     * @return      An upper case version of the passed in text.
+     */
+    string to_uppercase(const string &text);
 
-/**
- * Checks if a string contains an integer value.
- *
- * @param  text The text to check.
- * @return      True if the text contains a whole number (and only a whole
- *                   number).
- */
-bool is_integer(const string &text);
+    /**
+     * Checks if a string contains an integer value.
+     *
+     * @param  text The text to check.
+     * @return      True if the text contains a whole number (and only a whole
+     *                   number).
+     */
+    bool is_integer(const string &text);
 
-/**
- * Checks if a string contains a number.
- *
- * @param  text The text to check.
- * @return      True if the text contains a number (and only a number).
- */
-bool is_number(const string &text);
+    /**
+     * Checks if a string contains a number.
+     *
+     * @param  text The text to check.
+     * @return      True if the text contains a number (and only a number).
+     */
+    bool is_number(const string &text);
 
-/**
- * Convert the passed in string into an integer. This can fail in an error if
- * the value is not an integer, consider using `is_integer` to check before
- * converting a string.
- *
- * @param  text The text to convert.
- * @return      The integer value read from the text.
- */
-int convert_to_integer(const string &text);
+    /**
+     * Convert the passed in string into an integer. This can fail in an error if
+     * the value is not an integer, consider using `is_integer` to check before
+     * converting a string.
+     *
+     * @param  text The text to convert.
+     * @return      The integer value read from the text.
+     */
+    int convert_to_integer(const string &text);
 
-/**
- * Convert the passed in string into a double. This can fail in an error if
- * the value is not a number, consider using `is_number` to check before
- * converting a string.
- *
- * @param  text The text to convert.
- * @return      The double value read from the text.
- */
-double convert_to_double(const string &text);
+    /**
+     * Convert the passed in string into a double. This can fail in an error if
+     * the value is not a number, consider using `is_number` to check before
+     * converting a string.
+     *
+     * @param  text The text to convert.
+     * @return      The double value read from the text.
+     */
+    double convert_to_double(const string &text);
+}
 
 #endif /* basics_hpp */
