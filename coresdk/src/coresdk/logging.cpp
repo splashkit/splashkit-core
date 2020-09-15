4af42ba82f194b8a5c059a368d3d1c426dc308a8
Andrew Cain <macite@gmail.com>
2020-09-15 10:04:12 +1000

    QUALITY: Add missing namespace and correct formatting of new logging module

--- coresdk/src/coresdk/logging.cpp
+++ coresdk/src/coresdk/logging.cpp
@@ -1,38 +1,45 @@
 #include "logging.h"
 #include "terminal.h"
 
-LogLevel _log_level = DEBUG;
+namespace splashkit_lib
+{
 
-void log(LogLevel level, string message) {
-  switch (level) {
-  case INFO:
-    if (_log_level == DEBUG)
+  log_level _log_level = DEBUG;
+
+  void log(log_level level, string message)
+  {
+    switch (level)
     {
-      return;
+    case INFO:
+      if (_log_level == DEBUG)
+      {
+        return;
+      }
+    case DEBUG:
+      if (_log_level == WARNING)
+      {
+        return;
+      }
+      write("DEBUG: ");
+      break;
+    case WARNING:
+      if (_log_level == ERROR)
+      {
+        return;
+      }
+      write("WARNING: ");
+      break;
+    case ERROR:
+      write("ERROR: ");
       break;
     }
-  case DEBUG:
-    if (_log_level == WARNING) {
-      return;
-    }
-    write("DEBUG: ");
-    break;
-  case WARNING:
-    if (_log_level == ERROR) {
-      return;
-    }
-    write("WARNING: ");
-    break;
-  case ERROR:
-    write("ERROR: ");
-    break;
-  }
 
-  auto time = std::chrono::system_clock::now();
-  std::time_t c_time = std::chrono::system_clock::to_time_t(time);
-  string str_time = std::ctime(&c_time);
-  // Required to remove the new line character ctime decides to add
-  write(str_time.substr(0, str_time.length() - 1));
-  write(" ");
-  write(message);
-}
+    auto time = std::chrono::system_clock::now();
+    std::time_t c_time = std::chrono::system_clock::to_time_t(time);
+    string str_time = std::ctime(&c_time);
+    // Required to remove the new line character ctime decides to add
+    write(str_time.substr(0, str_time.length() - 1));
+    write(" ");
+    write(message);
+  }
+} // namespace splashkit_lib
--- coresdk/src/coresdk/logging.h
+++ coresdk/src/coresdk/logging.h
@@ -1,19 +1,35 @@
-#ifndef FIRESTORM_LOGGING
-#define FIRESTORM_LOGGING
+#ifndef logging_h
+#define logging_h
 
 #include "terminal.h"
 #include <chrono>
 #include <ctime>
 
-enum LogLevel {
-  INFO,
-  DEBUG,
-  WARNING,
-  ERROR
-};
+namespace splashkit_lib
+{
 
-extern LogLevel _log_level;
+  /**
+   *  
+   * @constant INFO     Output information to the message log
+   * @constant DEBUG    Output a debug message to the message log
+   * @constant WARNING  Output a warning message to the message log
+   * @constant ERROR    Output an error message to the message log
+   */
+  enum log_level {
+    INFO,
+    DEBUG,
+    WARNING,
+    ERROR
+  };
 
-void log(LogLevel level, string message);
+  /**
+   * Send a message to the message log. The message will be written if the log level for
+   * the program is set to display this.
+   * 
+   * @param level     The level of the message to log
+   * @param message   The message to be shown
+   */
+  void log(log_level level, string message);
 
+}
 #endif
