fdb7bc8f12b8fcdebaa396e45a4946d46ce183d9
Andrew Cain <macite@gmail.com>
2020-10-27 20:36:32 +1100

    TEST: Add unit tests for loading of system fonts

---
+++ coresdk/src/test/unit_tests/unit_test_text.cpp
@@ -0,0 +1,26 @@
+/**
+ * Text and font Unit Tests
+ */
+
+#include <vector>
+
+#include "catch.hpp"
+
+#include "text.h"
+#include "types.h"
+#include "backend_types.h"
+#include "utility_functions.h"
+
+using namespace splashkit_lib;
+
+TEST_CASE("can load system fonts", "[text]")
+{
+    font test;
+    test = load_font("Arial", "Arial");
+
+    SECTION("can load arial")
+    {
+        REQUIRE(test != nullptr);
+        REQUIRE(VALID_PTR(test, FONT_PTR));
+    }
+}
