699659dae241d61563eb55d1f9867bf6e232e578
Andrew Cain <macite@gmail.com>
2021-12-14 22:43:58 +1100

    docs: correct logging headerdocs

--- coresdk/src/coresdk/logging.h
+++ coresdk/src/coresdk/logging.h
@@ -17,13 +17,16 @@ namespace splashkit_lib
 {
 
     /**
-     *    defines the available log levels for log messages.
-     * @constant NONE         Output to the message log but without a specified level
-     * @constant INFO         Output information to the message log
-     * @constant DEBUG        Output a debug message to the message log
-     * @constant WARNING    Output a warning message to the message log
-     * @constant ERROR        Output an error message to the message log
-     * @constant FATAL        Output an error message to the message log
+     * Defines the available levels for log messages. This allows categorisation of log
+     * messages which can then be filtered allowing messages below a given level to be
+     * ignored.
+     * 
+     * @constant NONE    Output to the message log but without a specified level
+     * @constant INFO    Output information to the message log
+     * @constant DEBUG   Output a debug message to the message log
+     * @constant WARNING Output a warning message to the message log
+     * @constant ERROR   Output an error message to the message log
+     * @constant FATAL   Output an error message to the message log
      */
     enum log_level {
         NONE,
@@ -35,7 +38,8 @@ namespace splashkit_lib
     };
     
     /**
-     *    Defines the available modes of logging.
+     * Defines the available modes of logging, indicating where messages are written to.
+     * 
      * @constant NONE Set the logging mode to none for nothing to be logged to the console or a file.
      * @constant CONSOLE Ensure that output only directs to the on-screen, text-based console..
      * @constant FILE Ensure that output only directs to a text file..
@@ -49,21 +53,19 @@ namespace splashkit_lib
     };
     
     /**
-     * This Function is overloaded.
-     * changes the logging mode between either writing to a file or both a file and the text-based console.
+     * Initialises the logging mode between either writing to a file or both a file and the text-based console.
+     * 
      * @param app_name The name of the application being written requiring logging
-     * @param override_prev_log Determines whether or not a new logging session should override the existing file, if any.    Set this to false if you want new log messages to be appended to the bottom of the file; otherwise set it to true if you would like a new file to be created on top of the old one.  
+     * @param override_prev_log Determines whether or not a new logging session should override the existing file, if any. Set this to false if you want new log messages to be appended to the bottom of the file; otherwise set it to true if you would like a new file to be created on top of the old one.  
      * @param mode The mode of log output i.e. whether there should be output to the console, a text file, or both.  Pass your choice of mode variable in by reference.
      */
-        void init_custom_logger (string app_name, bool override_prev_log, log_mode mode);
+    void init_custom_logger (string app_name, bool override_prev_log, log_mode mode);
 
     /**
-     * This Function is overloaded.
-     * changes the logging mode for logged messages to be written to the text-based console.
-true if you would like a new file to be created on top of the old one.  
+     * Initialises the logging mode for logged messages to be written to the text-based console.
      * @param mode The mode of log output i.e. whether there should be output to the console, a text file, or both.  Pass your choice of mode variable in by reference.
      */
-        void init_custom_logger (log_mode mode);
+    void init_custom_logger (log_mode mode);
 
     /**
      * Send a message to the message log. The message will be written if the log level for
@@ -76,7 +78,7 @@ true if you would like a new file to be created on top of the old one.
     
     /**
      * Ensures propper memory clean-up prior to exit, if needed.  Used in sk_init_looging ().
-    */
+     */
     void close_log_process ();
 }
 #endif
