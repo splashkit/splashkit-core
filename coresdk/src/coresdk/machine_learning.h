3b7aac9ef785b8c8ea6d58c9335508d9641580d7
Harry <hdentry56@gmail.com>
2022-04-10 14:25:25 +1000

    fix: corrected format with header
    Moved functions into machine_learning.cpp and respective header file.
    Added some tests

---
+++ coresdk/src/coresdk/machine_learning.h
@@ -0,0 +1,183 @@
+#ifndef machine_learning_h
+#define machine_learning_h
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include "random.h"
+#include "terminal.h"
+#include "logging.h"
+using namespace std;
+
+namespace splashkit_lib
+{
+	class QValue;
+
+	class OutputFormat
+	{
+	public:
+		enum class Type
+		{
+			Position,
+			Number,
+			Category
+		};
+
+	private:
+		int board_size;
+		vector<Type> format;
+		vector<int> f_data;
+		vector<int> indexes;
+		int output_width = 0;
+
+	public:
+		OutputFormat(int board_size)
+		{
+			this->board_size = board_size;
+		}
+
+		int size() { return format.size(); }
+		int get_width() { return output_width; }
+
+		void add_type(Type type, int data)
+		{
+			format.push_back(type);
+			f_data.push_back(data);
+			indexes.push_back(output_width);
+			switch (type)
+			{
+			case Type::Number:
+				output_width++;
+				break;
+			case Type::Position:
+			case Type::Category:
+				output_width += data;
+				break;
+			}
+		}
+
+		int get_max_position(QValue &data, int index, vector<int> filter, bool random);
+
+		/*
+		float process_output_number(QValue &output, int index)
+		{
+			if (format[index] != Type::Number)
+				throw invalid_argument("Format at index is not of type Number!");
+			int out_index = indexes[index];
+			return output[out_index] * f_data[index];
+		} //*/
+	};
+
+	class QValue
+	{
+	private:
+		vector<float> value;
+		vector<int> indexes; // indexes to affect during reward/punishment
+	public:
+		QValue() {}
+		QValue(OutputFormat *format)
+		{
+			value = vector<float>(format->get_width(), 0.5f);
+		}
+
+		QValue(vector<float> value)
+		{
+			this->value = value;
+		}
+
+		// adds an index to be updated during reward/punishment
+		void to_update(int index)
+		{
+			indexes.push_back(index);
+		}
+
+		void update(float reward) 
+		{
+			for (int i = 0; i < indexes.size(); i++)
+			{
+				value[indexes[i]] += reward;
+			}
+			indexes.clear();
+		}
+
+		float operator[](int index) { return value[index]; }
+		string to_string() 
+		{ 
+			stringstream ss;
+			ss << "[ ";
+			for(float v : value)
+			{
+				ss << v << " ";
+			}
+			ss << "]";
+			return ss.str();
+		}
+	};
+
+	class QTable
+	{
+	private:
+		OutputFormat *format;
+		unordered_map<vector<bool>, QValue> q_table;
+	public:
+		QTable(OutputFormat *format)
+		{
+			this->format = format;
+		}
+
+		QValue get_q_value(vector<bool> key)
+		{
+			log(INFO, "QTable::get_q_value");
+			if (q_table.find(key) == q_table.end())
+			{
+				log(INFO, "QTable::get_q_value - creating new q_value");
+				q_table[key] = QValue(format);
+			}
+			return q_table[key];
+		}
+	};
+
+	class Game
+	{
+	public:
+		virtual int get_current_player() { return 0; }
+		virtual vector<float> score() { throw logic_error("score(): Function needs to be overridden; should return a vector with player indexes mapped to score at the end of the game"); }
+
+		// The number of different 'pieces' that can exist on the field throughout any game
+		// e.g. Tic-Tac-Toe [Empty + O + X = 2], Chess [Empty + (Pawn + Bishop + Knight + Rook + Queen + King) * 2 Players = 12]
+		// Pong [Empty + Ball/Paddle = 1]
+		virtual int get_max_board_index() { throw logic_error("get_max_board_index(): Function needs to be overridden; should return the highest number possible in get_board()"); }
+
+		// The number of tiles on the board e.g. Tic-Tac-Toe = 3*3 = 9, Chess = 8*8 = 64
+		virtual int get_board_size() { throw logic_error("get_board_size(): Function needs to be overridden; should return the length of get_board()"); }
+
+		virtual OutputFormat get_output_format() { throw logic_error("get_output_format(): Function needs to be overridden; should return an OutputFormat that can represent any possible move"); }
+		virtual vector<int> get_board() { throw logic_error("get_board(): Function needs to be overridden; should return a vector of all the tiles on the board"); }
+
+		virtual vector<int> get_possible_moves() { return get_possible_moves(get_board()); }
+		virtual vector<int> get_possible_moves(vector<int> board) { throw logic_error("get_possible_moves(): Function needs to be overridden; should return a vector of all the possible moves"); }
+		virtual void make_move(int move) { throw logic_error("make_move(): Function needs to be overridden; should take a move and update the board"); }
+		virtual bool step() { throw logic_error("step(): Function needs to be overridden; should return false if the game is over"); }
+		virtual int convert_output(QValue output, bool random) { throw logic_error("convert_output(): Function needs to be overridden; should return the index of the move that the output represents"); }
+
+		// convert board to categorical vector
+		vector<bool> convert_board()
+		{
+			vector<int> board = get_board();
+			vector<bool> new_board(board.size() * get_max_board_index(), false);
+			for (int i = 0; i < board.size(); i++)
+			{
+				if (board[i] != 0)
+				{
+					new_board[i * get_max_board_index() + board[i] - 1] = true;
+				}
+			}
+			return new_board;
+		}
+	};
+
+}
+
+#endif
--- coresdk/src/test/test_machine_learning.cpp
+++ coresdk/src/test/test_machine_learning.cpp
@@ -1,128 +1,14 @@
 #include "window_manager.h"
 #include "terminal.h"
+#include "logging.h"
 #include "random.h"
+#include "machine_learning.h"
 
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 using namespace std;
 using namespace splashkit_lib;
-
-class OutputFormat
-{
-public:
-	enum class Type
-	{
-		Position,
-		Number,
-		Category
-	};
-private:
-	int board_size;
-	vector<Type> format;
-	vector<int> f_data;
-	vector<int> indexes;
-	int output_width = 0;
-public:
-	OutputFormat(int board_size) 
-	{
-		this->board_size = board_size;
-	}
-
-	int size() { return format.size(); }
-	int get_width() { return output_width; }
-
-	void add_type(Type type, int data)
-	{
-		format.push_back(type);
-		f_data.push_back(data);
-		indexes.push_back(output_width);
-		switch (type)
-		{
-		case Type::Number:
-			output_width++;
-			break;
-		case Type::Position:
-		case Type::Category:
-			output_width += data;
-			break;
-		}
-	}
-
-	vector<float> process_output_position(vector<float> output, int index)
-	{
-		if (format[index] != Type::Position) throw invalid_argument("Format at index is not of type Position!");
-		vector<float> result(f_data[index]);
-		for (int i = 0; i < f_data[index]; i++)
-		{
-			result[i] = output[indexes[index] + i];
-		}
-		return result;
-	}
-
-	int get_max_position(QValue position_data, vector<int> filter, bool random) 
-	{
-		if (random) {
-			int out = filter[rnd(filter.size())];
-			// update q_table
-			position_data.to_update(out);
-			return out;
-		}
-		float max = position_data[filter[0]];
-		int max_pos = filter[0];
-		for (int i = 1; i < filter.size(); i++)
-		{
-			if (position_data[filter[i]] > max)
-			{
-				max = position_data[filter[i]];
-				max_pos = filter[i];
-			}
-		}
-		position_data.to_update(max_pos);
-		return max_pos;
-	}
-
-	float process_output_number(vector<float> output, int index)
-	{
-		if (format[index] != Type::Number) throw invalid_argument("Format at index is not of type Number!");
-		int out_index = indexes[index];
-		return output[out_index] * f_data[index];
-	}
-};
-
-class Game
-{
-public:
-	virtual int get_current_player() { return 0; }
-
-	// The number of different 'pieces' that can exist on the field throughout any game
-	// e.g. Tic-Tac-Toe [Empty + O + X = 2], Chess [Empty + (Pawn + Bishop + Knight + Rook + Queen + King) * 2 Players = 12]
-	// Pong [Empty + Ball/Paddle = 1]
-	virtual int get_max_board_index() { throw logic_error("get_max_board_index(): Function needs to be overridden; should return the highest number possible in get_board()"); }
-
-	// The number of tiles on the board e.g. Tic-Tac-Toe = 3*3 = 9, Chess = 8*8 = 64
-	virtual int get_board_size() { throw logic_error("get_board_size(): Function needs to be overridden; should return the length of get_board()"); }
-
-	virtual OutputFormat get_output_format() { throw logic_error("get_output_format(): Function needs to be overridden; should return an OutputFormat that can represent any possible move"); }
-	virtual vector<int> get_board() { throw logic_error("get_board(): Function needs to be overridden; should return a vector of all the tiles on the board"); }
-
-	virtual vector<int> get_possible_moves() { throw logic_error("get_possible_moves(): Function needs to be overridden; should return a vector of all the possible moves"); }
-	virtual void make_move(int move) { throw logic_error("make_move(): Function needs to be overridden; should take a move and update the board"); }
-	virtual bool step() { throw logic_error("step(): Function needs to be overridden; should return false if the game is over"); }
-	virtual int convert_output(QValue output, bool random) { throw logic_error("convert_output(): Function needs to be overridden; should return the index of the move that the output represents"); }
-
-	// convert board to categorical vector
-	vector<bool> convert_board() {
-		vector<int> board = get_board();
-		vector<bool> new_board(board.size() * get_max_board_index(), false);
-		for (int i = 0; i < board.size(); i++) {
-			if (board[i] != 0) {
-				new_board[i * get_max_board_index() + board[i] - 1] = true;
-			}
-		}
-		return new_board;
-	}
-};
 
 class TicTacToe : public Game
 {
@@ -162,11 +48,11 @@ public:
 	Board board;
 	Player current_player;
 	GameState state;
-	OutputFormat format;
+	OutputFormat format = OutputFormat(get_board_size());
 
 	int get_current_player() override { return (int)current_player; }
 	int get_max_board_index() override { return 2; } // O or X
-	int get_board_size() override { return 9; }		// 3x3 = 9
+	int get_board_size() override { return 9; }		 // 3x3 = 9
 	OutputFormat get_output_format() override { return format; }
 	vector<int> get_board() override
 	{
@@ -180,14 +66,17 @@ public:
 		}
 		return board_data;
 	}
+	int convert_output(QValue output, bool random) override
+	{
+		return format.get_max_position(output, 0, get_possible_moves(), random);
+	}
 
-	void init()
+	TicTacToe()
 	{
 		board = {{{Cell::Empty, Cell::Empty, Cell::Empty},
 				  {Cell::Empty, Cell::Empty, Cell::Empty},
 				  {Cell::Empty, Cell::Empty, Cell::Empty}}};
 
-		format = OutputFormat(get_board_size());
 		format.add_type(OutputFormat::Type::Position, get_board_size());
 
 		// When passing to AI
@@ -270,9 +159,18 @@ public:
 		{
 			for (int x = 0; x < 3; x++)
 			{
-				if (board.cells[y][x] == Cell::Empty) { write("_"); }
-				else if (board.cells[y][x] == Cell::X) { write("X"); }
-				else if (board.cells[y][x] == Cell::O) { write("O"); }
+				if (board.cells[y][x] == Cell::Empty)
+				{
+					write("_");
+				}
+				else if (board.cells[y][x] == Cell::X)
+				{
+					write("X");
+				}
+				else if (board.cells[y][x] == Cell::O)
+				{
+					write("O");
+				}
 			}
 			write_line();
 		}
@@ -281,9 +179,18 @@ public:
 	void draw_game()
 	{
 		draw_board();
-		if (state == GameState::X_Won) { write_line("X Won!"); }
-		else if (state == GameState::O_Won) { write_line("O Won!"); }
-		else if (state == GameState::Draw) { write_line("Draw!"); }
+		if (state == GameState::X_Won)
+		{
+			write_line("X Won!");
+		}
+		else if (state == GameState::O_Won)
+		{
+			write_line("O Won!");
+		}
+		else if (state == GameState::Draw)
+		{
+			write_line("Draw!");
+		}
 	}
 
 	vector<int> get_possible_moves() override
@@ -311,117 +218,114 @@ public:
 	}
 };
 
-class QValue
-{
-private:
-	vector<float> value;
-	vector<int> indexes; // indexes to affect during reward/punishment
-public:
-	QValue(OutputFormat format)
-	{
-		value = vector<float>(format.get_width(), 0.5f);
-	}
-
-	// adds an index to be updated during reward/punishment
-	void to_update(int index)
-	{
-		indexes.push_back(index);
-	}
-
-	float operator[](int index) { return value[index]; }
-};
-
-class QAgent 
-{
-private:
-	OutputFormat out_format;
-	float learning_rate = 0.1f;
-	float discount_factor = 0.9f;
-	float epsilon = 0.1f;
-
-	unordered_map<vector<bool>,QValue> q_table;
-public:
-	vector<QValue> q_history; // Store the q_values used for the last game
-
-	QAgent(unordered_map<vector<bool>,QValue> q_table, OutputFormat out_format)
-	{
-		this->q_table = q_table;
-		this->out_format = out_format;
-	}
-
-	QValue get_q_value(vector<bool> key)
-	{
-		if (q_table.find(key) == q_table.end())
-		{
-			q_table[key] = QValue(out_format);
-		}
-		return q_table[key];
-	}
-
-	int get_move(Game& game)
-	{
-		if (rnd() < epsilon)
-		{
-			vector<int> moves = game.get_possible_moves();
-			vector<bool> input = game.convert_board();
-			QValue q_output = get_q_value(input);
-			return game.convert_output(q_output, true);
-			return moves[rnd(moves.size())];
-		} // else {
-		vector<bool> input = game.convert_board();
-		QValue q_output = get_q_value(input);
-		return game.convert_output(q_output, false);
-	}
-};
-
-class QTrainer {
-	unordered_map<vector<bool>,QValue> q_table;
-
-	void train(Game game, int iterations)
-	{ 
-		for (int i = 0; i < iterations; i++)
-		{
-			Game game; // generate the game
-			vector<QAgent> agents; // generate the agents to play the game
-			while (game.step())
-			{
-				int move = agents[game.get_current_player()].get_move(game);
-				game.make_move(move);
-			}
-			// Update q_table
-			/*
-			vector<float> scores = game.score();
-			for (int i = 0; i < scores.size(); i++) {
-				float score = scores[i];
-				agents[i].reward(score);
-			} //*/
-		}
-	}
-};
-
 int random_agent_play(int posb_moves)
 {
-	if (posb_moves < 1) throw logic_error("No moves available; Game over?");
-	if (posb_moves == 1) return 0;
+	if (posb_moves < 1)
+		throw logic_error("No moves available; Game over?");
+	if (posb_moves == 1)
+		return 0;
 	return rnd(0, posb_moves - 1);
 }
 
-vector<float> q_agent_play(Game &game) {
+int q_agent_play(Game &game)
+{
 	OutputFormat format = game.get_output_format();
 	vector<bool> input = game.convert_board();
 
 	// vector<float> q_output = q_learning(input)
 	vector<float> q_output = {0.2, 0.9, 0, 0, 1, 0, 0, 0.9, 0.3};
 
-	vector<float> position_data = format.process_output_position(q_output, 0);
+	return game.convert_output(QValue(q_output), false);
+}
 
-	return position_data;
+bool test_q_table()
+{
+	bool passes = true;
+
+	TicTacToe game;
+	OutputFormat format = game.get_output_format();
+	QTable q_table = QTable(&format);
+	QValue test = q_table.get_q_value(game.convert_board());
+	if (test[0] != 0.5f)
+	{
+		passes = false;
+	}
+	write("Initial QValues = 0.5? ");
+	write(((test[0] == 0.5f) ? "true" : "false"));
+	write_line(" (" + to_string(test[0]) + ")");
+	write_line(test.to_string());
+
+	return passes;
+}
+
+bool test_q_value()
+{
+	bool passes = true;
+	const float F_ERR = 0.01f; // Float error for comparisons.
+
+	// Consider a right hand turn represented with possible actions LEFT=0 CENTER=1 or RIGHT=2
+	vector<float> val = {0, 0, 0};
+	QValue test = QValue(val);
+
+	// We take a left turn
+	test.to_update(0);
+	// We punish the left turn
+	test.update(-1);
+	if (abs(-1 - test[0]) > F_ERR || abs(0 - test[1]) > F_ERR || abs(0 - test[2]) > F_ERR)
+	{
+		passes = false;
+		write("Expected: {-1, 0, 0}, Actual: "); write_line(test.to_string());
+		log(WARNING, "LEFT turn failed");
+	}
+
+	// We take a LEFT CENTER turn
+	test.to_update(0);
+	test.to_update(1);
+	// We punish the center less
+	test.update(-0.7);
+	if (abs(-1.7 - test[0]) > F_ERR || abs(-0.7 - test[1]) > F_ERR || abs(0 - test[2]) > F_ERR)
+	{
+		passes = false;
+		write("Expected: {-1.7, -0.7, 0}, Actual: "); write_line(test.to_string());
+		log(WARNING, "LEFT CENTER turn failed");
+	}
+
+	// We take a RIGHT CENTER turn
+	test.to_update(1);
+	test.to_update(2);
+	// We reward a correct turn
+	test.update(1);
+	if (abs(-1.7 - test[0]) > F_ERR || abs(0.3 - test[1]) > F_ERR || abs(1 - test[2]) > F_ERR)
+	{
+		passes = false;
+		write("Expected: {-1.7, 0.3, 1}, Actual: "); write_line(test.to_string());
+		log(WARNING, "RIGHT CENTER turn failed");
+	}
+
+	// We take no action
+	test.update(999);
+	if (abs(-1.7 - test[0]) > F_ERR || abs(0.3 - test[1]) > F_ERR || abs(1 - test[2]) > F_ERR)
+	{
+		passes = false;
+		write("Expected: {-1.7, 0.3, 1}, Actual: "); write_line(test.to_string());
+		log(WARNING, "NULL update failed");
+	}
+
+	write("test_q_value: "); write_line(test.to_string());
+
+	return passes;
 }
 
 void run_machine_learning_test()
 {
+	log_level _log_level = INFO;
+	log_mode _log_mode = LOG_CONSOLE;
+	init_custom_logger(_log_mode);
+
+	test_q_table();
+	test_q_value();
+
 	TicTacToe game;
-	game.init();
 	game.draw_game();
 	while (game.state == TicTacToe::GameState::Playing)
 	{
@@ -429,10 +333,11 @@ void run_machine_learning_test()
 		if (game.current_player == TicTacToe::Player::X)
 		{
 			OutputFormat format = game.get_output_format();
-			vector<float> moves_weight = q_agent_play(game);
-			int ai_move = format.get_max_position(moves_weight, moves);
+			int ai_move = q_agent_play(game);
 			game.make_move(ai_move);
-		} else {
+		}
+		else
+		{
 			game.make_move(moves[random_agent_play(moves.size())]);
 		}
 	}
