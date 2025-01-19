16d02483e4812b9c948b19b26cf441b3e8534a6b
Matt439 <matt439@gmail.com>
2025-01-19 20:43:31 +1000

    remove collision_direction enum

--- coresdk/src/coresdk/collisions.h
+++ coresdk/src/coresdk/collisions.h
@@ -18,35 +18,6 @@
 namespace splashkit_lib
 {
     /**
-     *  This enumeration contains a list of directions that an
-     *  object can collide with another object. For example, a
-     *  collider object which is colliding with another object
-     *  on its top edge would have a collision direction of TOP.
-     *  
-     *  @constant TOP           The top of the object
-     *  @constant BOTTOM        The bottom of the object
-     *  @constant LEFT          The left of the object
-     *  @constant RIGHT         The right of the object
-     *  @constant TOP_LEFT      The top left of the object
-     *  @constant TOP_RIGHT     The top right of the object
-     *  @constant BOTTOM_LEFT   The bottom left of the object
-     *  @constant BOTTOM_RIGHT  The bottom right of the object
-     *  @constant NONE          No collision
-     */
-    enum collision_direction
-    {
-        TOP,
-        BOTTOM,
-        LEFT,
-        RIGHT,
-        TOP_LEFT,
-        TOP_RIGHT,
-        BOTTOM_LEFT,
-        BOTTOM_RIGHT,
-        NONE,
-    };
-    
-    /**
      * Tests if a bitmap drawn using the passed in translation matrix would draw a pixel
      * at the passed in point. Use to check collisions between a point and a bitmap.
      *
