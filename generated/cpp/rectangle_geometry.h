22483c28bf1378c4b01cd90377e2a85ca2b81383
omckeon <ordmckeon@gmail.com>
2025-02-26 15:11:09 +1100

    Update generate files

--- generated/cpp/rectangle_geometry.h
+++ generated/cpp/rectangle_geometry.h
@@ -19,16 +19,16 @@ rectangle rectangle_around(const circle &c);
 rectangle rectangle_around(const line &l);
 rectangle rectangle_around(const quad &q);
 rectangle rectangle_around(const triangle &t);
-float rectangle_bottom(const rectangle &rect);
+double rectangle_bottom(const rectangle &rect);
 point_2d rectangle_center(const rectangle &rect);
 rectangle rectangle_from(const point_2d pt, const double width, const double height);
 rectangle rectangle_from(const point_2d pt1, const point_2d pt2);
 rectangle rectangle_from(double x, double y, double width, double height);
-float rectangle_left(const rectangle &rect);
+double rectangle_left(const rectangle &rect);
 rectangle rectangle_offset_by(const rectangle &rect, const vector_2d &offset);
-float rectangle_right(const rectangle &rect);
+double rectangle_right(const rectangle &rect);
 string rectangle_to_string(const rectangle &rect);
-float rectangle_top(const rectangle &rect);
+double rectangle_top(const rectangle &rect);
 bool rectangles_intersect(const rectangle &rect1, const rectangle &rect2);
 
 #endif /* __rectangle_geometry_h */
