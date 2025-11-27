1a405ebf8d489a99f24f11c1113cac86d3c3a103
omckeon <ordmckeon@gmail.com>
2025-11-27 23:49:05 +1100

    Fix servo code for Pi 5

--- coresdk/src/backend/gpio_driver.cpp
+++ coresdk/src/backend/gpio_driver.cpp
@@ -40,6 +40,7 @@ namespace splashkit_lib
     // Add map to track items for RPi GPIO
     unordered_map<int, int> pin_modes;
     unordered_map<int, int> pwm_range;
+    unordered_map<int, int> pwm_pulsewidth;
     unordered_map<int, int> handle_channel;
 
     const int BCMpinData[] = {-1, -1, 2, -1, 3, -2, 4, 14, -2, 15, 17, 18, 27, -2, 22, 23, -1, 24, 10, -2, 9, 25, 11, 8, -2, 7, 0, 1, 5, -2, 6, 12, 13, -2, 19, 16, 26, 20, -2, 21};
@@ -228,12 +229,6 @@ namespace splashkit_lib
     {
         if (check_pi(pin, PWM_PIN))
         {
-            // Checks whether newly set range is a reasonable value
-            if (range <= 25 || range > 4096)
-            {
-                LOG(ERROR) << sk_gpio_error_message(PI_BAD_DUTYRANGE);
-                return;
-            }
 #ifdef RASPBERRY_PI_5
             // Save values to map to use for other functions (pigpio did this automatically)
             pinMode(pin, PWM_OUTPUT);
@@ -261,6 +256,7 @@ namespace splashkit_lib
             // Checks if range exists in the map of know PWM ranges
             if (range < 25)
             {
+                LOG(WARNING) << "Note: PWM range needs to be set before PWM frequency.";
                 LOG(ERROR) << sk_gpio_error_message(PI_BAD_DUTYRANGE);
                 return;
             }
@@ -609,8 +605,8 @@ namespace splashkit_lib
         if (check_pi(pin, PWM_PIN))
         {
 #ifdef RASPBERRY_PI_5
-            LOG(ERROR) << "Servo functionality not yet available on Raspberry Pi 5.";
-            return;
+            sk_set_pwm_dutycycle(pin, pulsewidth);
+            pwm_pulsewidth[pin] = pulsewidth;
 #else
             int result = set_servo_pulsewidth(pi, pin, pulsewidth);
             if (result < 0)
@@ -626,17 +622,16 @@ namespace splashkit_lib
         if (check_pi(pin, PWM_PIN))
         {
 #ifdef RASPBERRY_PI_5
-            LOG(ERROR) << "Servo functionality not yet available on Raspberry Pi 5.";
-            return -1;
+            int result = pwm_pulsewidth[pin];
 #else
             int result = get_servo_pulsewidth(pi, pin);
+#endif
             if (result < 0)
             {
                 LOG(ERROR) << sk_gpio_error_message(result);
                 return -1;
             }
             return result;
-#endif
         }
         return -1;
     }
